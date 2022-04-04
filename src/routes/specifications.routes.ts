054f0c84403b00f7e7eeefb8bb85f604b826bbb1
Roberto Torga <rtorga@gmail.com>
2022-04-04 00:20:32 -0300

    first commit

---
+++ src/routes/specifications.routes.ts
@@ -0,0 +1,9 @@
+import { Request, Response, Router } from 'express';
+
+import { createSpecificationController } from '../modules/cars/useCases/createSpecification';
+
+const specificationsRoutes = Router();
+
+specificationsRoutes.post('/', (request: Request, response: Response) => createSpecificationController.handle(request, response));
+
+export { specificationsRoutes };
