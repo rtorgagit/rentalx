57182ede014ab3475c41dd2f35d3befd82cbeae7
Roberto Torga <rtorga@gmail.com>
2022-04-08 22:50:33 -0300

    Changing Specification to typeorm

--- src/database/migrations/1649424436351-CreateSpecifications.ts
+++ src/database/migrations/1649424436351-CreateSpecifications.ts
@@ -1,11 +1,23 @@
-import {MigrationInterface, QueryRunner} from "typeorm";
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
 
 export class CreateSpecifications1649424436351 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table(
+        {
+          name: 'specifications',
+          columns: [
+            { name: 'id', type: 'uuid', isPrimary: true },
+            { name: 'name', type: 'varchar' },
+            { name: 'description', type: 'varchar' },
+            { name: 'created_at', type: 'timestamp', default: 'Now()' },
+          ],
+        },
+      ),
+    );
+  }
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-    }
-
-    public async down(queryRunner: QueryRunner): Promise<void> {
-    }
-
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('specifications');
+  }
 }
