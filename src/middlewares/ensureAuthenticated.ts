124fbddc2cd85c1bb24018cfaa9bcd72d953c341
Roberto Torga <rtorga@gmail.com>
2022-04-11 23:07:36 -0300

    Update-Upload Avatar

--- src/middlewares/ensureAuthenticated.ts
+++ src/middlewares/ensureAuthenticated.ts
@@ -24,7 +24,9 @@ export async function ensureAuthenticated(request: Request, response: Response, 
     if (!user) {
       throw new AppError('User not found!', 401);
     }
-
+    request.user = {
+      id: user_id,
+    };
     next();
   } catch (err) {
     throw new AppError('Invalid token', 401);
--- src/modules/accounts/useCases/updateUserAvatar/updateUserAvatarUseCase.ts
+++ src/modules/accounts/useCases/updateUserAvatar/updateUserAvatarUseCase.ts
@@ -1,4 +1,32 @@
+import { inject, injectable } from 'tsyringe';
+
+import { AppError } from '../../../../errors/AppError';
+import { deleteFile } from '../../../../utils/file';
+import { IUsersRepository } from '../../repositories/IUsersRepository';
+
+interface IRequest {
+  user_id: string;
+  avatar_file: string;
+}
+
+@injectable()
 class UpdateUserAvatarUseCase {
-  async execute() { }
+  constructor(
+    @inject('UsersRepository')
+    private usersRepository: IUsersRepository,
+  ) { }
+  async execute({ user_id, avatar_file }: IRequest): Promise<void> {
+    const user = await this.usersRepository.findByID(user_id);
+    if (!user) {
+      throw new AppError('User not found', 400);
+    }
+    if (user.avatar) {
+      await deleteFile(`./tmp/avatar/${user.avatar}`);
+    }
+
+    user.avatar = avatar_file;
+
+    await this.usersRepository.create(user);
+  }
 }
 export { UpdateUserAvatarUseCase };
