c30275ce79741adeaf3bb740943aa6dc45337a5f
Roberto Torga <rtorga@gmail.com>
2022-06-05 13:27:46 -0300

    feat: add rentals feature - completed

--- src/modules/rentals/useCases/createRental/CreateRentalUseCase.ts
+++ src/modules/rentals/useCases/createRental/CreateRentalUseCase.ts
@@ -1,5 +1,8 @@
+import { inject, injectable } from 'tsyringe';
+
 import { Rental } from '@modules/rentals/infra/typeorm/entities/Rental';
 import { IRentalsRepository } from '@modules/rentals/repositories/IRentalsRepository';
+import { IDateProvider } from '@shared/container/providers/DateProvider/IDateProvider';
 import { AppError } from '@shared/errors/AppError';
 
 interface IRequest {
@@ -8,12 +11,18 @@ interface IRequest {
   expected_return_date: Date;
 }
 
+@injectable()
 class CreateRentalUseCase {
   constructor(
+    @inject('RentalsRepository')
     private rentalsRepository: IRentalsRepository,
+    @inject('DayjsDateProvider')
+    private dateProvider: IDateProvider,
   ) { }
 
   async execute({ user_id, car_id, expected_return_date }: IRequest): Promise<Rental> {
+    const minimumRentalTime = 24; // 24 hours - 1 day
+
     const carUnavailable = await this.rentalsRepository.findOpenRentalByCar(car_id);
 
     if (carUnavailable) {
@@ -24,6 +33,13 @@ class CreateRentalUseCase {
 
     if (rentalOpenToUser) {
       throw new AppError('There is a rental in progress to the user');
+    }
+
+    const dateNow = this.dateProvider.dateNow();
+    const compare = this.dateProvider.compareInHours(dateNow, expected_return_date);
+
+    if (compare < minimumRentalTime) {
+      throw new AppError('Invalid return time, less than 24 hours');
     }
 
     const rental = await this.rentalsRepository.create({
